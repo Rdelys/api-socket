62bd9447c31f2b16e85c3b730bd53f17cc3eb708
unknown <rdauphinelys@gmail.com>
2025-07-29 19:16:46 +0300

    son

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 // Socket.IO avec CORS sécurisé pour ton domaine prod et chemin /socket.io
 const io = socketIO(server, {
   cors: {
-    origin: "https://livebeautyofficial.com", // autorise uniquement ce domaine
+    origin: "http://localhost:3000", // autorise uniquement ce domaine
     methods: ["GET", "POST"],
     credentials: true
   },
